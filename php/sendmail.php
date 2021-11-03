55c302210a36fa44cd2212eef475603ddd302b94
fokilloki <81332509+fokilloki@users.noreply.github.com>
2021-11-03 19:47:53 +0300

    Add files via upload

---
+++ php/sendmail.php
@@ -0,0 +1,57 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php'
+	require 'phpmailer/src/PHPMailer.php'
+
+	$mail = new PHPMailer(true);
+	$mail - >CharSet = 'UTF-8';
+	$mail - >setLanguage('ru', 'phpmailer/language/');
+	$mail - >IsHtml(true);
+
+	// От кого письмо
+	$mail - >setFrom('info@magrimebel.ru', 'Шкафы купе');
+
+	$mail - >addAdress('fominkk@bk.ru');
+
+	$mail - >Subject = 'Поступил заказ';
+
+
+
+
+	// Тело письма
+	$body = '<h1>Письмо</h1>'
+
+	if(trim(!empty($_POST['username']))) {
+		$body.='<p><stong>Имя:</stong>' .$_POST['username'].'</p>';
+	}
+	if(trim(!empty($_POST['userphone']))) {
+		$body.='<p><stong>Телефон:</stong>' .$_POST['userphone'].'</p>';
+	}
+	if(trim(!empty($_POST['usermail']))) {
+		$body.='<p><stong>E-Mail:</stong>' .$_POST['usermail'].'</p>';
+	}
+	if(trim(!empty($_POST['userfile']))) {
+		$body.='<p><stong>Эскиз:</stong>' .$_POST['userfile'].'</p>';
+	}
+	if(trim(!empty($_POST['message']))) {
+		$body.='<p><stong>Сообщение:</stong>' .$_POST['message'].'</p>';
+	}
+
+	$mail->Body = $body;
+
+	// Отправка
+	if(!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+
+
+?>
